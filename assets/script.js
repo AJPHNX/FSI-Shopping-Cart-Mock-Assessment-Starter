0bfe0c1cea41182fcd50caa73e27f5a1f084ecfc
AJ Phoenix <ajphnx_nu@AJs-MacBook-Pro.local>
2022-01-29 14:52:58 -0500

    Remove button removes item

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,7 @@
 let downButton = document.querySelector('#quantity-down')
 let upButton = document.querySelector('#quantity-up')
+let rmvButton = document.querySelector('.remove')
+let item = document.querySelector('.cart-item')
 
 //let originalQuantity = document.querySelector('.totalQuantity')
 function updateQuantity(displayQuantity){
@@ -7,6 +9,7 @@ function updateQuantity(displayQuantity){
     quantity.innerHTML = displayQuantity
 }
 let quantity = 1
+
 downButton.addEventListener('click', function(e){
     if (quantity>0){
        quantity--
@@ -22,3 +25,10 @@ upButton.addEventListener('click', function(e){
         updateQuantity(`Quantity: ${quantity}`)
 })
 
+rmvButton.addEventListener('click',function(e){
+    item.style.visibility = 'hidden'
+    if (quantity-- <1){
+        window.alert('Cart Empty')
+    } else {updateQuantity(`Quantity: ${quantity}`)}
+    console.log('Removed')
+}) 
