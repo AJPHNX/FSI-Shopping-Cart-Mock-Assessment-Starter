8a6e6db74a7cdb041478067602ca164027204b53
AJ Phoenix <ajphnx_nu@AJs-MacBook-Pro.local>
2022-01-29 17:24:36 -0500

    Updates total product price on first. Second item just sits so far

--- assets/script.js
+++ assets/script.js
@@ -2,15 +2,41 @@ let downButton = document.querySelector('#quantity-down')
 let upButton = document.querySelector('#quantity-up')
 let rmvButton = document.querySelector('.remove')
 let item = document.querySelector('.cart-item')
+let itemName = document.querySelector('h3')
+let totalItemPrice = document.querySelector('.total-price')
+//let itemTwoPrice = 15
+//let itemOnePrice = 1500
 
 //let originalQuantity = document.querySelector('.totalQuantity')
 function updateQuantity(displayQuantity){
     let quantity = document.querySelector ('.total-quantity')
     quantity.innerHTML = displayQuantity
 }
+function updateItemPrice(displayPrice){
+    let priceText = document.querySelector ('.total-price')
+    priceText.innerHTML = displayPrice
+}
+//price select
+function itemPriceSelect(e){
+    if (e.innerHTML == 'Pet Rock: limestone'){
+        let newPrice = '15'
+    }else if(e.innerHTML == 'Pet Rock: Bertrandite'){
+        let newPrice = '1500'
+    }
+    console.log(newPrice)
+    return  newPrice
+}
+
 let quantity = 1
+if (quantity){
+    let quantPrice = quantity * 15
+    updateItemPrice(`Total Price available at checkout: $ ${quantPrice}`)
+}else{ quantPrice = 0
+
+}
 
 downButton.addEventListener('click', function(e){
+    let quantPrice = quantity * 15
     if (quantity>0){
        quantity--
        console.log(quantity)
@@ -18,11 +44,15 @@ downButton.addEventListener('click', function(e){
         window.alert("nothing in the cart")
     }
         updateQuantity(`Quantity: ${quantity}`)
+        updateItemPrice(`Total Price available at checkout: $ ${quantPrice}`)
 })
 upButton.addEventListener('click', function(e){
        quantity++
-       console.log(quantity)
+       let quantPrice = quantity * 15
         updateQuantity(`Quantity: ${quantity}`)
+        console.log(quantity)
+        console.log('total Price:'+ quantPrice)
+        updateItemPrice(`Total Price available at checkout: $ ${quantPrice}`)
 })
 
 rmvButton.addEventListener('click',function(e){
